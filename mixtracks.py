f41d5a53784664454fdafe487a38edceb226a1d1
vagrant <toanbkhn95@gmail.com>
2018-06-05 05:56:43 +0000

    Add filter files

--- mixtracks.py
+++ mixtracks.py
@@ -25,30 +25,69 @@ def get_prefix(list_of_tracks, grammar):
     return name
 
 
-def mix(list_of_tracks, num_files_per_group, num_plays,
-    num_copies=1, prefix='', to_mp3=False, shuffled='', silence_padding='0'):
-    #type_file = 'Accent' if not grammar else 'Grammar'
-    #artist = 'Accent' if not grammar else 'Grammar'
-    #album = 'Accent Training' if not grammar else 'Grammar Training'
-    artist = ''
-    album = ''
-    type_file = ''
+def process(exceptions):
+    results = []
+    for f in exceptions.split('.'):
+        if ':' in f:
+            for i in range(int(f.split(':')[0]), int(f.split(':')[1]) + 1):
+                results.append(i)
+        else:
+            results.append(int(f))
+    return results
+
+
+def find_exceptions(track):
+    add = 0
+    if track.find('+') != -1:
+        add = 1
+    elif track.find('-') != -1:
+        add = -1
+    if add == 1:
+        track = track.split('+')
+        return add, int(track[0][1:]), process(track[1])
+    elif add == -1:
+        track = track.split('-')
+        return add, int(track[0][1:]), process(track[1])
+    return add, int(track[1:]), []
+
+
+def filter_files(list_of_tracks, shuffled):
     input_files = []
+    artist = ''
     for track in list_of_tracks:
         type_file = MAPPING[track[0]]
         if type_file not in artist.split('_'):
         	artist += type_file + '_'
         sub_input_files = []
+        add, track_number, exceptions = find_exceptions(track)
         for f in sorted(os.listdir(type_file + '/' + type_file + 'EN/')):
             if not (f[-3:] == 'mp3' or f[-3:] == 'wav'): continue
             if type_file == 'Grammar': u = f[1:4]
             elif type_file == 'Accent': u = f[6:9]
             elif type_file == 'Extra': u = f[1:4]
-            if u == '%03d' % (int(track[1:])):
-                sub_input_files.append(type_file +'/' + type_file +'EN/' + f)
+            if u == '%03d' % (track_number):
+                if add == 1 and int(f[f.find('-') + 1:-6]) in exceptions:
+                    sub_input_files.append(type_file + '/' + type_file + 'EN/' + f)
+                    continue
+                elif add == -1 and int(f[f.find('-') + 1:-6]) in exceptions:
+                    continue
+                elif add == 0 or add == -1:
+                    sub_input_files.append(type_file + '/' + type_file + 'EN/' + f)
         if shuffled == "group":
             shuffle(sub_input_files)
         input_files.extend(sub_input_files)
+    for f in input_files:
+        print (f)
+    return input_files, artist, type_file
+
+
+def mix(list_of_tracks, num_files_per_group, num_plays,
+    num_copies=1, prefix='', to_mp3=False, shuffled='', silence_padding='0'):
+    #type_file = 'Accent' if not grammar else 'Grammar'
+    #artist = 'Accent' if not grammar else 'Grammar'
+    #album = 'Accent Training' if not grammar else 'Grammar Training'
+    album = ''
+    input_files, artist, type_file = filter_files(list_of_tracks, shuffled)
     if shuffled == "all": shuffle(input_files)
     if prefix == '' or prefix == None:
         prefix = get_prefix(list_of_tracks, type_file)
@@ -67,40 +106,3 @@ def mix(list_of_tracks, num_files_per_group, num_plays,
         make_track(result, name)
         convert_mp3(to_mp3, name, dir_name.replace("wav", "mp3"), artist, album)
     rmtree('output_' + artist)
-
-
-def create_accent_grammar(list_of_tracks, num_files_per_group, num_plays,
-    num_copies=1, prefix='', to_mp3=False, artist='Accent', album='Accent Training', shuffled='', silence_padding='0'):
-    #type_file = 'Accent' if not grammar else 'Grammar'
-    #artist = 'Accent' if not grammar else 'Grammar'
-    #album = 'Accent Training' if not grammar else 'Grammar Training'
-    type_file = artist
-    input_files = []
-    for track in list_of_tracks:
-        sub_input_files = []
-        for f in sorted(os.listdir(type_file + '/' + type_file + 'EN/')):
-            if not (f[-3:] == 'mp3' or f[-3:] == 'wav'): continue
-            if type_file == 'Grammar': u = f[1:4]
-            elif type_file == 'Accent': u = f[6:9]
-            elif type_file == 'Extra': u = f[1:4]
-            if u == '%03d' % (track):
-                sub_input_files.append(type_file +'/' + type_file +'EN/' + f)
-        if shuffled == "group":
-            shuffle(sub_input_files)
-        input_files.extend(sub_input_files)
-    if shuffled == "all": shuffle(input_files)
-    if prefix == '' or prefix == None:
-        prefix = get_prefix(list_of_tracks, type_file)
-    if num_files_per_group == 0:
-        num_files_per_group = get_num_files(len(input_files))
-    generate_from_list_of_files(input_files, type_file, False, silence_padding)
-    files = ['output_' + type_file + '/' + f.split('/')[-1][:-6] + type_file + f.split('/')[-1][-4:] for f in input_files]
-    # Shuffle files
-    for copies in range(int(num_copies)):
-        result = shuffle_track(files, num_plays, num_files_per_group)
-        dir_name = OUTPUT_ALL + '(wav)/' + sub_directory() + '/' + type_file
-        makedir(dir_name)
-        name = get_name(dir_name, prefix, num_plays, silence_padding)
-        make_track(result, name)
-        convert_mp3(to_mp3, name, dir_name.replace("wav", "mp3"), artist, album)
-    rmtree('output_' + type_file)
